b6881006a23ff4cce5731ee933fba03c2d677a8d
IkeAlmighty <IsaacYates7@gmail.com>
2021-07-18 15:50:02 -0500

    first commit

---
+++ pages/grayowl/index.js
@@ -0,0 +1,18 @@
+import marked from "marked";
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+export default function Index() {
+  const [markdown, setMarkdown] = useState("Loading...");
+  useEffect(() => {
+    axios.get("/grayowl.md").then((res) => {
+      setMarkdown(res.data);
+      document.getElementById("markdown").innerHTML = marked(markdown);
+    });
+  });
+  return (
+    <div>
+      <div id="markdown"></div>
+    </div>
+  );
+}
