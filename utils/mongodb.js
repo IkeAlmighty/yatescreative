33529a5f16964466edc7c7a697f5abcbc7f2e2e7
IkeAlmighty <IsaacYates7@gmail.com>
2021-10-17 11:14:23 -0500

    updated mongodb driver

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,25 +1,18 @@
-import {MongoClient} from "mongodb";
+import { MongoClient } from "mongodb";
 
 const MONGO_URI = process.env.MONGO_URI;
 
-let conn = null;
-let db = null;
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-export async function connectToDatabase() {
-  if (!conn) {
-    try {
-      await new MongoClient.connect(MONGO_URI, options).then((client) => {
-        conn = client;
-        db = client.db();
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  }
+let clientPromise = global.clientPromise;
 
-  return {conn, db};
+if (!clientPromise) {
+  let client = new MongoClient(MONGO_URI, options);
+  global.clientPromise = client.connect();
+  clientPromise = global.clientPromise;
 }
+
+export default clientPromise;
