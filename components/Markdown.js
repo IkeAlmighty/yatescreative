7f7dc2b0b19f5e514b30ab6a63a1633c3f5acd8f
Izik <IsaacYates7@gmail.com>
2022-03-23 12:49:45 -0500

    added markdown component back in

---
+++ components/Markdown.js
@@ -0,0 +1,23 @@
+import marked from "marked";
+export default function Markdown({
+  markdown,
+  editable,
+  onEdit,
+  editorClassName,
+}) {
+  function getMarkdownText() {
+    const rawMarkup = marked(markdown);
+    return { __html: rawMarkup };
+  }
+
+  if (editable)
+    return (
+      <textarea
+        className={editorClassName}
+        onChange={(e) => onEdit(e.target.value)}
+        placeholder="The WRITE ZONE"
+        value={markdown}
+      />
+    );
+  else return <div dangerouslySetInnerHTML={getMarkdownText()} />;
+}
