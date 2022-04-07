0df70dfc42d00bdc920ffd174b82218387830a9a
Izik <IsaacYates7@gmail.com>
2022-04-07 14:29:48 -0500

    added some pages

---
+++ pages/booking.js
@@ -0,0 +1,43 @@
+import { useEffect, useRef } from "react";
+import Navigation from "../components/Navigation";
+
+export default function Booking() {
+  const calendlyContainer = useRef();
+
+  useEffect(() => {
+    const calendlyWidgetScript = document.createElement("script");
+    calendlyWidgetScript.setAttribute(
+      "src",
+      "https://assets.calendly.com/assets/external/widget.js"
+    );
+    calendlyWidgetScript.setAttribute("type", "text/javascript");
+    calendlyWidgetScript.setAttribute("async", true);
+
+    calendlyContainer.current.appendChild(calendlyWidgetScript);
+  }, []);
+
+  return (
+    <>
+      <div className="with-navbar">
+        {/* <!-- Calendly inline widget begin --> */}
+        <div ref={calendlyContainer}>
+          <div
+            className="calendly-inline-widget"
+            data-url="https://calendly.com/isaacyates"
+            style={{ minWidth: "320px", height: "630px" }}
+          ></div>
+
+          {/* 
+          The script tag is added with js on page load - useEffect(()=>{...}, []).
+          I do it this way because there is a problem with reloading Script from
+          next/script, after the initial page load.
+          */}
+
+          {/* <!-- Calendly inline widget end --> */}
+        </div>
+      </div>
+
+      <Navigation />
+    </>
+  );
+}
---
+++ pages/thoughts/index.js
@@ -0,0 +1,33 @@
+import Navigation from "../../components/Navigation";
+import clientPromise from "../../utils/mongodb";
+
+export default function Index({ articleData }) {
+  return (
+    <div className="content-container">
+      {articleData.map((articleData) => (
+        <div key={articleData._id}></div>
+      ))}
+
+      <Navigation />
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const client = await clientPromise;
+
+  const articleData = await client
+    .db()
+    .collection("thoughts")
+    .find({})
+    .project({
+      _id: { $toString: "$_id" },
+      text: true,
+      timestamp: true,
+      title: true,
+      imageKey: true,
+    })
+    .toArray();
+
+  return { props: { articleData } };
+}
