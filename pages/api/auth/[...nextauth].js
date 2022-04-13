ae2df78aa6224f80e09670e2c799913dbc8dee6f
Izik <IsaacYates7@gmail.com>
2022-04-13 18:03:56 -0500

    added sha256 package to encrypt passwords, added custom credentials

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,37 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 import clientPromise from "../../../lib/mongodb";
+import sha256 from "sha256";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
+    CredentialsProvider({
+      name: "Credentials",
+
+      credentials: {
+        username: { label: "username", type: "text", placeholder: "hi u" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const { username, password } = credentials;
+
+        const mongoClient = await clientPromise;
+        const user = await mongoClient
+          .db()
+          .collection("users")
+          .findOne({ username, password: sha256(password) });
+
+        console.log("crypto: ", sha256(password));
+        console.log(user);
+
+        return user.isAdmin ? user : null;
+      },
     }),
     // ...add more providers here
   ],
