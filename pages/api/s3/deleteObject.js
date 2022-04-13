c61f3016e0c6708fe047ab854341fb1e3b7ebec4
Izik <IsaacYates7@gmail.com>
2022-04-13 16:33:33 -0500

    FIXED: incorrect import path

--- pages/api/s3/deleteObject.js
+++ pages/api/s3/deleteObject.js
@@ -1,5 +1,5 @@
 import AWS from "aws-sdk";
-import { getSession } from "next-auth/react";
+import { getSession } from "next-auth/client";
 export default async function handler(req, res) {
   const session = await getSession({ req });
 
