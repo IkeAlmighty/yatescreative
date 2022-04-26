201b45e107b262e412d72e8a79e0c1990a76082f
Izik <IsaacYates7@gmail.com>
2022-04-26 14:42:08 -0500

    update portfolio page to actually show a portfolio

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,8 +1,49 @@
 import Navigation from "../../lib/components/Navigation";
+import PortfolioCard from "../../lib/components/portfolio/PortfolioCard";
+import Link from "next/link";
 
 export default function Portfolio() {
   return (
     <div>
+      <div className="with-navbar">
+        <div className="my-6 max-w-lg md:p-0 p-2 mx-auto">
+          <div className="pt-10 pb-6 mb-20 border-b-2 border-black">
+            <h1>Isaac Yates</h1>
+            <div>Website Development Portfolio</div>
+            <a href="/resume.pdf" download>
+              <div className="text-blue-600">(click here for resume)</div>
+            </a>
+          </div>
+
+          <p className="text-left tracking-wide">
+            In addition to the following websites, yatescreative.com (this
+            website) has public source code that can be viewed on github
+            via&nbsp;
+            <Link href="https://github.com/IkeAlmighty/yatescreative/">
+              <a className="text-blue-500 inline-block">this link</a>
+            </Link>
+          </p>
+
+          <PortfolioCard
+            deploymentLink="https://norman.events"
+            imgSrc="/headernormanevents.png"
+            title="norman.events"
+            description="Norman Events is a local event aggregator for my home town, Norman,
+            Oklahoma."
+            githubLink="https://github.com/IkeAlmighty/norman.events"
+          />
+          <hr className="my-20" />
+          <PortfolioCard
+            deploymentLink="https://grayowl.coffee"
+            imgSrc="/headergrayowlcoffee.png"
+            title="grayowl.coffee"
+            description="A simple landing page for the coffee shop I work at, Gray Owl Coffee."
+            githubLink="https://github.com/IkeAlmighty/grayowlcoffee"
+          />
+          <hr />
+        </div>
+      </div>
+
       <Navigation />
     </div>
   );
---
+++ public/resume.md
@@ -0,0 +1,48 @@
+## Isaac Yates
+
+###### Website Developer, Programmer, Barista
+
+<div style="position: fixed; right: 0; top: 5px; font-size: 12px; text-align: right;">
+isaacyates.creative@gmail.com </br>
+(405) 202-4472
+
+</div>
+
+**View my portfolio on at [yatescreative.com/portfolio](https://yatescreative/portfolio)**
+
+**_General Skills:_**  
+_Git, NextJs, ReactJs, SvelteJs, MongoDB, Restful APIs_
+
+# Job History
+
+---
+
+## ReactJs Developer
+
+##### Askme Tutoring (August 2020 - December 2021)
+
+- ReactJS
+- Bootstrap
+- Redux
+
+## Website Designer
+
+##### Oklahoma Water Survey (August 2018 - June 2019)
+
+- Javascript, HTML, & CSS Snippets
+- Adobe CMS
+- Wordpress CMS
+
+## Private Programming Tutor
+
+##### (August 2017 - May 2018)
+
+- Introductory Java
+- Structures and Abstractions
+
+## Programming Intern
+
+##### Kyungpook Nat'l University (March 2017 - May 2017)
+
+- Java TCP Sockets
+- Language Barrier (Korean & English)
