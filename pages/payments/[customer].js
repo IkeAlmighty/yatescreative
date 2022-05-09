2436d95067eacbf01928be405daff20e2527bdf9
Izik <IsaacYates7@gmail.com>
2022-05-09 15:54:08 -0500

    added joke customer payment page for Jada

---
+++ pages/payments/[customer].js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Customer() {
+  return (
+    <div className="mx-auto">
+      <Image layout="fill" src="/arrow2heart.gif" alt="<3" />
+    </div>
+  );
+}
